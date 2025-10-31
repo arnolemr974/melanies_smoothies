eaf3a90a7578946b9b560b0af755d693cc52da7f
arnolemr974 <arnolemr@gmail.com>
2025-10-31 11:01:59 +0100

    Update streamlit_app.py
    back again

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,12 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
 pd_df=my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
       'Choose up to 5 ingredients:' 
@@ -38,6 +43,7 @@ if ingredients_list:
     st.write(ingredients_string) 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                 values ('"""+ ingredients_string + """','"""+name_on_order+"""')"""
-	time_to_insert = st.button('Submit Order')
+    time_to_insert = st.button('Submit Order')
     if time_to_insert:
         st.success('Your Smoothie is ordered!', icon="✅")
+#st.text(smoothiefroot_response.json())
