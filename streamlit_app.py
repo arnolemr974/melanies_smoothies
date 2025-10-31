eddcca9d2b4d49aea3e3467d6d0c59dfd78ec986
arnolemr974 <arnolemr@gmail.com>
2025-10-31 08:27:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ if ingredients_list:
 
     if time_to_insert:
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"
-st.text(smoothiefroot_response)
+	import requests
+	smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"
+	st.text(smoothiefroot_response)
 
